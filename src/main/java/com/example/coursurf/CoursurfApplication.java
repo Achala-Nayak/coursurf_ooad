c21c83b945b12cc934768a49c23767b3ce64742f
Achala-Nayak <achala.nayak2@gmail.com>
2024-04-12 23:05:22 +0530

    Initial commit

---
+++ src/main/java/com/example/coursurf/CoursurfApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursurf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursurfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursurf/CoursurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursurf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
