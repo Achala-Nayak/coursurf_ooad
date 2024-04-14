1c585c6d9ba540f567f99520520ecc047709e611
Achala-Nayak <achala.nayak2@gmail.com>
2024-04-14 16:39:43 +0530

    Mapping of pages- Achala Nayak

--- src/main/java/com/example/coursurf/controllers/Homecontroller.java
+++ src/main/java/com/example/coursurf/controllers/Homecontroller.java
@@ -5,8 +5,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Homecontroller {
+
     @GetMapping("/")
-    public String home(){
-        return "index"; 
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/courses")
+    public String courses() {
+        return "courses";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/team")
+    public String team() {
+        return "team";
+    }
+
+    @GetMapping("/testimonial")
+    public String testimonial() {
+        return "testimonial";
+    }
+
+    @GetMapping("/404")
+    public String error404() {
+        return "404";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
     }
 }
